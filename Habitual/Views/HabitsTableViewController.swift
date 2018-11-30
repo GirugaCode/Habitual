e2625358a51ad29512da168a9d479d8bd0a019af
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-11-29 21:12:25 -0800

    [add] UIBarButton to add Hello World

--- Habitual/Views/HabitsTableViewController.swift
+++ Habitual/Views/HabitsTableViewController.swift
@@ -9,22 +9,44 @@
 import UIKit
 
 class HabitsTableViewController: UITableViewController {
+    
+    var names: [String] = ["Alan", "Adriana", "Adam", "Anne", "Mitchell", "Dani"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupNavBar()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell
+        if let dequeueCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            cell = dequeueCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        let name = names[indexPath.row]
+        cell.textLabel?.text = name
+        return cell
+    }
+}
 
+extension HabitsTableViewController {
+    
+    func setupNavBar() {
+        title = "Habitual"
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(pressAddHabit(_:)))
+        navigationItem.rightBarButtonItem = addButton
+    }
+    
+    @objc func pressAddHabit(_ sender: UIBarButtonItem) {
+        names.insert("Hello, World!", at: 0)
+        let topIndexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [topIndexPath], with: .automatic)
+    }
 }
